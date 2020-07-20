f5e1a7de2a4a1f5833a43f36c98cf63342002343
Anrijs <anrijs@localhost>
2020-07-20 16:45:18 +0300

    map: go to edit from map view

--- map.php
+++ map.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Vēstures kartes</title>
+    <title>Calibration map</title>
     <link rel="stylesheet" href="assets/css/leaflet.css" />
     <script src="assets/js/leaflet.js"></script>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
@@ -31,13 +31,26 @@ var map_osm = L.tileLayer('//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxNativeZoom: 18,
 });
 
+var map_esri = L.tileLayer('//server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+        maxZoom: 22,
+        maxNativeZoom: 18,
+    });
+
 var baseMaps = {
-  "OSM": map_osm,
+    "OSM": map_osm,
+    "ESRI Sat": map_esri
 };
 
 var map = L.map('map', {maxZoom:22}).setView([56.75,24], 8);
 
 baseMaps["OSM"].addTo(map);
+
+var overlayMaps = {
+  // add your own ovelays...
+};
+
+L.control.layers(baseMaps, overlayMaps).addTo(map);
 
 $('.leaflet-container').css('cursor','crosshair');
 
@@ -45,6 +58,8 @@ $('.leaflet-container').css('cursor','crosshair');
   require_once 'lib/fn.php';
 
   $done = getDoneMaps();
+  $boxnum = 0;
+  $extra = "";
   foreach ($done as $map) {
 
     $polypts = array();
@@ -55,9 +70,12 @@ $('.leaflet-container').css('cursor','crosshair');
       $polypts[] = "[".$ptlat.",".$ptlon."]";
     }
 
-    echo "L.polygon([";
+    echo "var popupText = '<b>" . $map["name"] . "</b><br><a href=\"#\" onclick=\"bbox".$boxnum.".removeFrom(map);\">Hide</a> | <a href=\"calibrate.php?map=" . $map["name"] . "\">Edit</a>';"; 
+    echo "var bbox" . $boxnum . " = L.polygon([";
     echo implode(",",$polypts);
-    echo"]).bindTooltip(\"" . $map["name"] . "\", {direction:'center'}).addTo(map);";
+    echo"], {fillOpacity: 0.05, weight: 1.5 " . $extra . "}).bindTooltip(\"" . $map["name"] . "\", {direction:'center'}).bindPopup(popupText).addTo(map);";
+
+    ++$boxnum;
   }
 ?>
 
